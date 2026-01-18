94013ee177b7c8e1fe91191a3b49e550bab6569b
shellikerishukla-04dot <shellikerishukla@gmail.com>
2026-01-18 10:19:33 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,12 +4,13 @@ def celsius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
 if __name__ == "__main__":
+    try:
+        c = float(sys.argv[1])
+    except IndexError:
+        c = float(input("Enter temperature in Celsius: "))
+    except ValueError:
+        print("Please enter a valid number.")
+        sys.exit(1)
 
-    if len(sys.argv) > 1:
-        celsius = float(sys.argv[1])   # Jenkins parameter
-    else:
-        celsius = 25.0                 # Default value
+    print("Fahrenheit:", celsius_to_fahrenheit(c))
 
-    fahrenheit = celsius_to_fahrenheit(celsius)
-    print("Celsius:", celsius)
-    print("Fahrenheit:", fahrenheit)
